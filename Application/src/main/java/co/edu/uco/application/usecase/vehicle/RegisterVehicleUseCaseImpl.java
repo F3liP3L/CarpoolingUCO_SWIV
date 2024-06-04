f1dd1d34d98ef34349abeeb8471ab28f2aa25fc3
Juan Felipe Cardona <juan.cardona1338@uco.net.co>
2024-06-04 13:53:04 -0600

    :sparkles: feat (RegisterVehicleUseCase) agrega lógica para crear conductor por vehículo cuando registro un vehículo.

--- Application/src/main/java/co/edu/uco/application/usecase/vehicle/RegisterVehicleUseCaseImpl.java
+++ Application/src/main/java/co/edu/uco/application/usecase/vehicle/RegisterVehicleUseCaseImpl.java
@@ -3,8 +3,10 @@ package co.edu.uco.application.usecase.vehicle;
 import co.edu.uco.application.specification.impl.vehicle.ValidVehicleSpecification;
 import co.edu.uco.crosscutting.exception.GeneralException;
 import co.edu.uco.crosscutting.util.UtilUUID;
+import co.edu.uco.entity.DriverPerVehicleEntity;
 import co.edu.uco.entity.VehicleEntity;
 import co.edu.uco.port.input.bussiness.vehicle.RegisterVehicleUseCase;
+import co.edu.uco.port.output.repository.DriverPerVehicleRepository;
 import co.edu.uco.port.output.repository.VehicleRepository;
 import co.edu.uco.util.exception.CarpoolingCustomException;
 import jakarta.transaction.Transactional;
@@ -16,10 +18,12 @@ import org.springframework.stereotype.Service;
 public class RegisterVehicleUseCaseImpl implements RegisterVehicleUseCase {
 
     private final VehicleRepository repository;
+    private final DriverPerVehicleRepository driverPerVehicleRepository;
     private final ValidVehicleSpecification specification;
 
-    public RegisterVehicleUseCaseImpl(VehicleRepository repository, ValidVehicleSpecification specification) {
+    public RegisterVehicleUseCaseImpl(VehicleRepository repository, DriverPerVehicleRepository driverPerVehicleRepository, ValidVehicleSpecification specification) {
         this.repository = repository;
+        this.driverPerVehicleRepository = driverPerVehicleRepository;
         this.specification = specification;
     }
 
@@ -28,7 +32,9 @@ public class RegisterVehicleUseCaseImpl implements RegisterVehicleUseCase {
         try {
             domain.setId(UtilUUID.getUtilUUID().getNewUUID());
             specification.isSatisfyBy(domain);
+            DriverPerVehicleEntity driverPerVehicle = DriverPerVehicleEntity.build(domain, "Activo");
             repository.save(domain);
+            driverPerVehicleRepository.save(driverPerVehicle);
         } catch (CarpoolingCustomException exception) {
             throw exception;
         } catch (GeneralException exception) {
